a3036be8cd3af9e16b44715f7c5af1b8505507d6
Williamonyango <ojwangwiliam@gmail.com>
2025-08-14 11:37:13 +0300

    changed the hero section layout

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./HeroSection.css";
 import heroImg from "../../assets/images/hero6.jpeg";
-import circleImg1 from "../../assets/images/hero6.jpeg";
 import circleImg from "../../assets/images/hero.jpeg";
 import { useNavigate } from "react-router-dom";
 
@@ -10,12 +9,6 @@ const HeroSection = () => {
 
   return (
     <section className="hero" style={{ backgroundImage: `url(${heroImg})` }}>
-      {/* Decorative circles */}
-      {/* <img
-        src={circleImg}
-        alt="decorative"
-        className="circle circle-top-right"
-      /> */}
       <img
         src={circleImg}
         alt="decorative"
@@ -24,8 +17,12 @@ const HeroSection = () => {
 
       <div className="hero-overlay"></div>
       <div className="hero-content">
-        <h1 className="hero-title">Building Brands,</h1>
-        <p className="hero-subtitle">Empowering Teams</p>
+        <h1 className="hero-title">
+          Bringing brands <br /> closer to customers,
+        </h1>
+        <h1 className="hero-subtitle">
+          and empowering teams every step of the way.
+        </h1>
         <div className="hero-buttons">
           <button
             href="#services"
@@ -34,10 +31,7 @@ const HeroSection = () => {
           >
             Our Services
           </button>
-          <button
-            className="button2"
-            onClick={() => navigate("/contact")}
-          >
+          <button className="button2" onClick={() => navigate("/contact")}>
             Get a Quote
           </button>
         </div>
